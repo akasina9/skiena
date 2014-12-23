de9d44194692f8827557fa75e77a12dc039a771a
Abhishek Kasina <kasina.abhishek@gmail.com>
2014-12-23 00:32:41 +0000

    Invoking classes for the utilities package

--- chapter2/selection_sort.py
+++ chapter2/selection_sort.py
@@ -1,3 +1,5 @@
+from utilities import process_input
+
 def selection_sort(input_list): # {{{
     '''
     Given a list of numbers, returns them in sorted order
@@ -12,8 +14,6 @@ def selection_sort(input_list): # {{{
 	    if input_list[current_index] < input_list[min_index]:
 		min_index = current_index
 	
-	print index, min_index
-	
 	if not min_index == index:
 	    input_list[index], input_list[min_index] = input_list[min_index], input_list[index]
     
@@ -22,22 +22,6 @@ def selection_sort(input_list): # {{{
 
 
 if __name__ == "__main__":
-    def get_input_list(): # {{{
-	'''
-	Get the list of numbers as a list
-	'''
-	import sys, ast
-	input_list = []
-	for i in range(1, len(sys.argv)):
-	    try:
-		num_val = ast.literal_eval(sys.argv[i])
-		input_list.append(ast.literal_eval(sys.argv[i]))
-	    except Exception as e:
-		print 'EXCEPTION:', sys.argv[i], ' is not a number.'
-		return
-	return input_list
-    # }}}
-    
-    input_list = get_input_list()
+    input_list = process_input.get_numeric_list()
     if input_list:
         print selection_sort(input_list)
